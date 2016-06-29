5279f6260a82b43eef00668d4ec662b68c1792c9
Rommert de Bruijn <rommertdebruijn@gmail.com>
2016-06-29 20:06:12 +0200

    setting up Numbers

---
+++ src/main/java/org/ai/NumbersApp.java
@@ -0,0 +1,13 @@
+package org.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class NumbersApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
